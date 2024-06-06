81d9a5abe27472fcc115e6797d024e7d88de95c9
Hemmerson Luis Barros da Rosa <hemmerson@users.noreply.github.com>
2024-06-06 19:03:08 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
